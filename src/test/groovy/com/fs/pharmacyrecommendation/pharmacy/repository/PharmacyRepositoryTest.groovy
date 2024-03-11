99a8d722c08752394f21a92da2c96aa34d5c3c2f
IronKim <tankokim@gmail.com>
2024-03-12 01:04:20 +0900

    추가 테스트를 위해 setup 함수 추가
    각 테스트가 시작되기전 데이터를 초기화 하기 위해 setup함수를 추가하였다.

--- src/test/groovy/com/fs/pharmacyrecommendation/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/fs/pharmacyrecommendation/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired
 
 class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
 
+    def setup() {
+        pharmacyRepository.deleteAll()
+    }
+
     @Autowired
     private PharmacyRepository pharmacyRepository
 
@@ -31,6 +35,29 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
         result.getPharmacyName() == name
         result.getLatitude() == latitude
         result.getLongitude() == longitude
+    }
+
+    def "PharmacyRepository saveAll"() {
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        pharmacyRepository.saveAll(Arrays.asList(pharmacy))
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.size() == 1
+
     }
 
 }
