8ae2a2b9b1568fd0d49ecfe2deba5cfa99c467eb
IronKim <tankokim@gmail.com>
2024-03-12 21:26:37 +0900

    Spring retry 구현 및 검증
    API의 1번의 네트워크 호출 실패로 서비스의 비지니스 로직을 모두 실패처리하거나 fallback 처리하는것은 몇 번 다시 호출하는 것보다 큰 리소스 낭비가 될 수 있다.
    Spring retry를 이용해 여러번 다시 재시도를 할 수 있다.
    RetryTemplate와 @EnableRetry어노테이션을 사용하는 2가지 방법이 있다.
    
    mockWebServer를 이용해 서버를 모킹하여 retry를 테스트한다.

--- src/main/java/com/fs/pharmacyrecommendation/api/service/KakaoAddressSearchService.java
+++ src/main/java/com/fs/pharmacyrecommendation/api/service/KakaoAddressSearchService.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.RestTemplate;
@@ -24,6 +27,11 @@ public class KakaoAddressSearchService {
     @Value("${kakao.rest.api.key}")
     private String kakaoRestApiKey;
 
+    @Retryable(
+            retryFor = {RuntimeException.class}, // RuntimeException 발생 시 재시도
+            maxAttempts = 2, // 최대 2번 재시도
+            backoff = @Backoff(delay = 2000) // 2초 간격으로 재시도
+    )
     public KakaoApiResponseDto requestAddressSearch(String address) {
 
         if(ObjectUtils.isEmpty(address)) return null; // 주소가 없으면 null 반환
@@ -37,6 +45,12 @@ public class KakaoAddressSearchService {
 
         //kakao api 호출
         return  restTemplate.exchange(uri, HttpMethod.GET, httpEntity, KakaoApiResponseDto.class).getBody();
+    }
+
+    @Recover // 재시도 횟수를 모두 소진한 경우 실행
+    public KakaoApiResponseDto recover(RuntimeException e, String address) {
+        log.error("All the retries failed. address: {}, error: {}", address, e.getMessage());
+        return null;
     }
 
 }
