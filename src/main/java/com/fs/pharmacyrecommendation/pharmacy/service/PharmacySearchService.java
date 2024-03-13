66de1b59ee9372e3cc872b816d7a45a2a8721d6b
IronKim <tankokim@gmail.com>
2024-03-13 19:47:45 +0900

    Redis를 이용한 성능 개선
    약국 검색할 때 redis에 데이터가 먼저 있으면 redis에서 꺼내오고 아니면 db에서 데이터를 꺼내온다.

--- src/main/java/com/fs/pharmacyrecommendation/pharmacy/service/PharmacySearchService.java
+++ src/main/java/com/fs/pharmacyrecommendation/pharmacy/service/PharmacySearchService.java
@@ -1,5 +1,6 @@
 package com.fs.pharmacyrecommendation.pharmacy.service;
 
+import com.fs.pharmacyrecommendation.pharmacy.cache.PharmacyRedisTemplateService;
 import com.fs.pharmacyrecommendation.pharmacy.dto.PharmacyDto;
 import com.fs.pharmacyrecommendation.pharmacy.entity.Pharmacy;
 import lombok.RequiredArgsConstructor;
@@ -15,10 +16,16 @@ import java.util.stream.Collectors;
 public class PharmacySearchService {
 
     private final PharmacyRepositoryService pharmacyRepositoryService;
+    private final PharmacyRedisTemplateService pharmacyRedisTemplateService;
 
     public List<PharmacyDto> searchPharmacyDtoList() {
 
         // redis
+        List<PharmacyDto> pharmacyDtoList = pharmacyRedisTemplateService.findAll();
+        if(!pharmacyDtoList.isEmpty()) {
+            log.info("redis findAll success!");
+            return pharmacyDtoList;
+        }
 
         //db
         return pharmacyRepositoryService.findAll().stream().map(this::convertToPharmacyDto).collect(Collectors.toList());
